77a92c2dc0854459a37feb2f2ec300fb004ca668
Cob007 <godwill2beracah@outlook.com>
2024-04-05 14:34:59 -0400

    highlight update added

--- bwb-client/src/pages/Homepage/Homepage.jsx
+++ bwb-client/src/pages/Homepage/Homepage.jsx
@@ -1,28 +1,27 @@
 import "./Homepage.scss";
 import BWBBanner from "../../assets/svg/BWB-Banner.png";
 import MapBanner from "../../assets/svg/map.png";
-import FooterBanner from '../../assets/svg/Footer.png'
-import Section from '../../assets/svg/Section.png'
-import PlayerCardDisplay from '../../components/PlayerCardDisplay/PlayerCardDisplay'
+import FooterBanner from "../../assets/svg/Footer.png";
+import Section from "../../assets/svg/Section.png";
+import PlayerCardDisplay from "../../components/PlayerCardDisplay/PlayerCardDisplay";
 import Partners from "../../components/Partners/Partners";
+import Highlights from "../../components/Highlights/Highlights";
 
 const Homepage = () => {
+  return (
+    <main className="container">
+        <Highlights />
+      <img src={BWBBanner} alt="Image url src" />
+      <div>
+        <PlayerCardDisplay />
+        <Partners />
+      </div>
 
-    return (
-        <main className="container">
-
-
-            <img src={BWBBanner} alt="Image url src" />
-            <div>
-                <PlayerCardDisplay />
-                <Partners />
-            </div>
-
-            <img src={Section} alt="Image url src" />
-            <img src={MapBanner} alt="Image url src" />
-            <img src={FooterBanner} alt="Image url src" />
-        </main>
-    );
+      <img src={Section} alt="Image url src" />
+      <img src={MapBanner} alt="Image url src" />
+      <img src={FooterBanner} alt="Image url src" />
+    </main>
+  );
 };
 
 export default Homepage;
